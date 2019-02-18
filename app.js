641a2cab3e81d2cff01c7d63c8fc8ebed6382c4d
chengwenhao <chengwenhao@idaddy.cn>
2019-02-18 16:25:45 +0800

    增加handlebars模板引擎、处理静态文件

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
 const express = require('express');
-
 let app = express();
+
+let handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 2333);
 
+app.use(express.static(__dirname + '/public'));
+
+// Virtual data
+const fortunes = [
+  'javascript',
+  'node',
+  'express',
+  'ruby',
+  'goland',
+  'java',
+  'angualar',
+  'vue',
+  'react',
+  'webpack',
+  'gulp',
+  'commonjs',
+  'amd',
+  'cmd',
+  'es6',
+  'koa2',
+  'eggjs',
+  'mongodb',
+  'redis',
+];
+
 app.get('/', (req, res) => {
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+  res.render('home');
 })
 
 app.get('/about', (req, res) => {
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
+  let randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+  res.render('about', { fortunes: randomFortune });
 })
 
 // 定制404页面
 app.use((req, res) => {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 not found');
+  res.render('404');
 })
 
 // 定制500页面
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 Server Error');
+  res.render(500);
 })
 
 app.listen(app.get('port'), () => {
